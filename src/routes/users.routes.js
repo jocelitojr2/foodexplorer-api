bc1cb5cdabd7f5e808d9827a516820ad3788601f
Jocelito Junior <jocelito64@hotmail.com>
2024-05-17 14:35:51 -0300

    :sparkles: feat: User Validated

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const uploadConfig = require("../configs/upload");
 
 const UsersController = require('../controllers/UsersController');
+const UsersValidatedController = require('../controllers/UsersValidatedController');
 const UserAvatarController = require('../controllers/UserAvatarController');
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
@@ -10,9 +11,11 @@ const usersRoutes = Router();
 const upload = multer(uploadConfig.MULTER); 
 
 const usersController = new UsersController();
+const usersValidatedController = new UsersValidatedController();
 const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.get("/validated", ensureAuthenticated, usersValidatedController.index);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
 usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
 
